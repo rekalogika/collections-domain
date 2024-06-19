03e055e1df02006863a74522cf16b5516293c993
root <1102197+priyadi@users.noreply.github.com>
2024-06-19 06:48:02 +0000

    add `SafePageableCollection` and `SafeReadablePageableCollection` (#19)

---
+++ src/SafeCriteriaRecollection.php
@@ -0,0 +1,139 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of rekalogika/collections package.
+ *
+ * (c) Priyadi Iman Nurcahyo <https://rekalogika.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Rekalogika\Domain\Collections;
+
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\Common\Collections\Order;
+use Doctrine\Common\Collections\ReadableCollection;
+use Doctrine\Common\Collections\Selectable;
+use Rekalogika\Contracts\Collections\SafeReadablePageableCollection;
+use Rekalogika\Domain\Collections\Common\CountStrategy;
+use Rekalogika\Domain\Collections\Common\Exception\UnexpectedValueException;
+use Rekalogika\Domain\Collections\Common\Trait\CountableTrait;
+use Rekalogika\Domain\Collections\Common\Trait\ItemsWithSafeguardTrait;
+use Rekalogika\Domain\Collections\Common\Trait\PageableTrait;
+use Rekalogika\Domain\Collections\Common\Trait\SafeReadableCollectionTrait;
+use Rekalogika\Domain\Collections\Trait\RecollectionTrait;
+
+/**
+ * @template TKey of array-key
+ * @template T
+ * @implements SafeReadablePageableCollection<TKey,T>
+ */
+class SafeCriteriaRecollection implements SafeReadablePageableCollection, \Countable
+{
+    /** @use RecollectionTrait<TKey,T> */
+    use RecollectionTrait;
+
+    /** @use PageableTrait<TKey,T> */
+    use PageableTrait;
+
+    /** @use ItemsWithSafeguardTrait<TKey,T> */
+    use ItemsWithSafeguardTrait;
+
+    /** @use SafeReadableCollectionTrait<TKey,T> */
+    use SafeReadableCollectionTrait;
+
+    use CountableTrait;
+
+    /**
+     * @var ReadableCollection<TKey,T>&Selectable<TKey,T>
+     */
+    private readonly ReadableCollection&Selectable $collection;
+
+    private readonly Criteria $criteria;
+
+    /**
+     * @param ReadableCollection<TKey,T> $collection
+     * @param int<1,max> $itemsPerPage
+     * @param null|int<0,max> $count
+     * @param null|int<1,max> $softLimit
+     * @param null|int<1,max> $hardLimit
+     */
+    public function __construct(
+        ReadableCollection $collection,
+        ?Criteria $criteria = null,
+        private readonly int $itemsPerPage = 50,
+        private readonly CountStrategy $countStrategy = CountStrategy::Restrict,
+        private ?int &$count = null,
+        private readonly ?int $softLimit = null,
+        private readonly ?int $hardLimit = null,
+        private readonly ?bool $strict = null,
+    ) {
+        // save collection
+
+        if (!$collection instanceof Selectable) {
+            throw new UnexpectedValueException('The wrapped collection must implement the Selectable interface.');
+        }
+
+        $this->collection = $collection;
+
+        // save criteria
+
+        $criteria = clone ($criteria ?? Criteria::create());
+
+        if (\count($criteria->orderings()) === 0) {
+            $criteria->orderBy(['id' => Order::Descending]);
+        }
+
+        $this->criteria = $criteria;
+    }
+
+    /**
+     * @param null|Collection<TKey,T> $collection
+     * @param null|int<1,max> $itemsPerPage
+     * @param null|int<0,max> $count
+     * @param null|int<1,max> $softLimit
+     * @param null|int<1,max> $hardLimit
+     */
+    protected function createFrom(
+        ?ReadableCollection $collection = null,
+        ?Criteria $criteria = null,
+        ?int $itemsPerPage = 50,
+        ?CountStrategy $countStrategy = CountStrategy::Restrict,
+        ?int &$count = null,
+        ?int $softLimit = null,
+        ?int $hardLimit = null,
+        ?bool $strict = null,
+    ): static {
+        $count = $count ?? $this->count;
+
+        // @phpstan-ignore-next-line
+        return new static(
+            collection: $collection ?? $this->collection,
+            criteria: $criteria ?? $this->criteria,
+            itemsPerPage: $itemsPerPage ?? $this->itemsPerPage,
+            countStrategy: $countStrategy ?? $this->countStrategy,
+            count: $count,
+            softLimit: $softLimit ?? $this->softLimit,
+            hardLimit: $hardLimit ?? $this->hardLimit,
+            strict: $strict ?? $this->strict,
+        );
+    }
+
+    /**
+     * @return int<0,max>
+     */
+    private function getRealCount(): int
+    {
+        $count = $this->collection->matching($this->criteria)->count();
+
+        if ($count > 0) {
+            return $count;
+        }
+
+        return 0;
+    }
+}
---
+++ src/SafeRecollection.php
@@ -0,0 +1,188 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of rekalogika/collections package.
+ *
+ * (c) Priyadi Iman Nurcahyo <https://rekalogika.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Rekalogika\Domain\Collections;
+
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\Common\Collections\Order;
+use Doctrine\Common\Collections\Selectable;
+use Rekalogika\Contracts\Collections\SafePageableCollection;
+use Rekalogika\Domain\Collections\Common\Configuration;
+use Rekalogika\Domain\Collections\Common\CountStrategy;
+use Rekalogika\Domain\Collections\Common\Exception\UnexpectedValueException;
+use Rekalogika\Domain\Collections\Common\Trait\CountableTrait;
+use Rekalogika\Domain\Collections\Common\Trait\ItemsWithSafeguardTrait;
+use Rekalogika\Domain\Collections\Common\Trait\PageableTrait;
+use Rekalogika\Domain\Collections\Common\Trait\SafeReadableCollectionTrait;
+use Rekalogika\Domain\Collections\Common\Trait\SafeWritableCollectionTrait;
+use Rekalogika\Domain\Collections\Trait\RecollectionTrait;
+
+/**
+ * @template TKey of array-key
+ * @template T
+ * @implements SafePageableCollection<TKey,T>
+ */
+class SafeRecollection implements SafePageableCollection, \Countable
+{
+    /** @use RecollectionTrait<TKey,T> */
+    use RecollectionTrait;
+
+    /** @use PageableTrait<TKey,T> */
+    use PageableTrait;
+
+    /** @use ItemsWithSafeguardTrait<TKey,T> */
+    use ItemsWithSafeguardTrait;
+
+    /** @use SafeWritableCollectionTrait<TKey,T> */
+    use SafeWritableCollectionTrait;
+
+    /** @use SafeReadableCollectionTrait<TKey,T> */
+    use SafeReadableCollectionTrait;
+
+    use CountableTrait;
+
+    /**
+     * @var Collection<TKey,T>&Selectable<TKey,T>
+     */
+    private readonly Collection&Selectable $collection;
+
+    /**
+     * @var array<string,Order>
+     */
+    private readonly array $orderBy;
+
+    private readonly Criteria $criteria;
+
+    /**
+     * @param Collection<TKey,T> $collection
+     * @param null|array<string,Order>|string $orderBy
+     * @param int<1,max> $itemsPerPage
+     * @param null|int<0,max> $count
+     * @param null|int<1,max> $softLimit
+     * @param null|int<1,max> $hardLimit
+     */
+    public function __construct(
+        Collection $collection,
+        array|string|null $orderBy = null,
+        private readonly int $itemsPerPage = 50,
+        private readonly CountStrategy $countStrategy = CountStrategy::Restrict,
+        private ?int &$count = null,
+        private readonly ?int $softLimit = null,
+        private readonly ?int $hardLimit = null,
+        private readonly ?bool $strict = null,
+    ) {
+        // handle collection
+
+        if (!$collection instanceof Selectable) {
+            throw new UnexpectedValueException('The wrapped collection must implement the Selectable interface.');
+        }
+
+        $this->collection = $collection;
+
+        // handle orderBy
+
+        if ($orderBy === null) {
+            $orderBy = $this->getDefaultOrderBy();
+        }
+
+        if (\is_string($orderBy)) {
+            $orderBy = [$orderBy => Order::Ascending];
+        }
+
+        $this->orderBy = $orderBy;
+
+        $this->criteria = Criteria::create()->orderBy($this->orderBy);
+    }
+
+    /**
+     * @return array<string,Order>|string
+     */
+    protected function getDefaultOrderBy(): array|string
+    {
+        return Configuration::$defaultOrderBy;
+    }
+
+    /**
+     * @param null|Collection<TKey,T> $collection
+     * @param null|array<string,Order>|string $orderBy
+     * @param null|int<1,max> $itemsPerPage
+     * @param null|int<0,max> $count
+     * @param null|int<1,max> $softLimit
+     * @param null|int<1,max> $hardLimit
+     */
+    protected function createFrom(
+        ?Collection $collection = null,
+        array|string|null $orderBy = null,
+        ?int $itemsPerPage = 50,
+        ?CountStrategy $countStrategy = CountStrategy::Restrict,
+        ?int &$count = null,
+        ?int $softLimit = null,
+        ?int $hardLimit = null,
+        ?bool $strict = null,
+    ): static {
+        $count = $count ?? $this->count;
+
+        // @phpstan-ignore-next-line
+        return new static(
+            collection: $collection ?? $this->collection,
+            orderBy: $orderBy ?? $this->orderBy,
+            itemsPerPage: $itemsPerPage ?? $this->itemsPerPage,
+            countStrategy: $countStrategy ?? $this->countStrategy,
+            count: $count,
+            softLimit: $softLimit ?? $this->softLimit,
+            hardLimit: $hardLimit ?? $this->hardLimit,
+            strict: $strict ?? $this->strict,
+        );
+    }
+
+    /**
+     * @param null|int<0,max> $count
+     * @return SafeCriteriaRecollection<TKey,T>
+     */
+    protected function withCriteria(
+        Criteria $criteria,
+        CountStrategy $countStrategy = CountStrategy::Restrict,
+        ?int &$count = null,
+    ): SafeCriteriaRecollection {
+        // if $criteria has no orderings, add the current ordering
+        if (\count($criteria->orderings()) === 0) {
+            $criteria = $criteria->orderBy($this->orderBy);
+        }
+
+        return new SafeCriteriaRecollection(
+            collection: $this->collection,
+            criteria: $criteria,
+            itemsPerPage: $this->itemsPerPage,
+            countStrategy: $countStrategy,
+            count: $count,
+            softLimit: $this->softLimit,
+            hardLimit: $this->hardLimit,
+            strict: $this->strict,
+        );
+    }
+
+    /**
+     * @return int<0,max>
+     */
+    private function getRealCount(): int
+    {
+        $count = $this->collection->count();
+
+        if ($count > 0) {
+            return $count;
+        }
+
+        return 0;
+    }
+}
